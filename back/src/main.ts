8252048a4b11f4d3b3dc52ff5d8cffa1b5b6fc54
httpsJadson <dev.jadson27@gmail.com>
2026-05-16 14:45:01 -0300

    feat: add swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configurar CORS para aceitar requisições do frontend
+  app.enableCors({
+    origin: [
+      process.env.FRONT_END_URL,
+    ],
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization',
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: false,
+    }),
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('DriveWise: Dirija com sabedoria')
+    .setDescription('Documentação da API de DriveWise: Dirija com sabedoria')
+    .setVersion('1.3')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
